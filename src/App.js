0bac1ce21ff7416039cc987a63afca4ecb4da667
Santi Bello <santidebello@gmail.com>
2019-04-24 20:52:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello React!!
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+class TypesOfFood extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='App-header'>
+        <h1 >Types of Food:</h1>
+        {/* change code below this line */}
+        <p >This is React, Lovin it!</p>
+        {/* change code above this line */}
+      </div>
+    );
+  }
+};
+
+
+export default TypesOfFood;
