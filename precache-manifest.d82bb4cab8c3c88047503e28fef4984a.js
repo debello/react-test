152e8620579a54f9d0f67fed1f943264fa242910
debello <42223093+debello@users.noreply.github.com>
2019-08-06 18:56:36 +0200

    Updates

---
+++ precache-manifest.d82bb4cab8c3c88047503e28fef4984a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-test/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "70e7ee56d816de41e7e57d7c74f75c74",
+    "url": "/React-test/static/media/catsearch.70e7ee56.jpg"
+  },
+  {
+    "revision": "e2467bd001a14c99d08a",
+    "url": "/React-test/static/js/runtime~main.3793cc85.js"
+  },
+  {
+    "revision": "795f1a656db65d6da58d",
+    "url": "/React-test/static/js/main.794c0dda.chunk.js"
+  },
+  {
+    "revision": "6cbf32e6e7458f998581",
+    "url": "/React-test/static/js/2.5646c6a5.chunk.js"
+  },
+  {
+    "revision": "795f1a656db65d6da58d",
+    "url": "/React-test/static/css/main.a46022ef.chunk.css"
+  },
+  {
+    "revision": "c5aed416b3ec61c12ebb48294f329267",
+    "url": "/React-test/index.html"
+  }
+];
